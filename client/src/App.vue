b72f8cc6ddc7d05a214c2a2c1424c67377837ae0
larsdjursner <larsdjursner@gmail.com>
2022-06-27 00:14:44 +0200

    Update App.vue
    router

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,17 +1,7 @@
 <template>
-  <h1 class="text-3xl font-bold underline">Hello world!</h1>
-  <p>{{ store.getCount }}</p>
-  <p>{{ store.getCountTimesTwo }}</p>
-  <button @click="store.increment">increment</button>
-  <button @click="store.randomizeCounter">randomize</button>
+  <router-view />
 </template>
 
 <script>
-import { useStore } from "./stores/store"
-
-export default {
-  data: () => ({
-    store: useStore(),
-  }),
-}
+export default {}
 </script>
