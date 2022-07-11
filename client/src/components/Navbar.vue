90a6aef207a0f2ce6f8036a43e7e743182cf88d5
Lars Djursner <larsdjursner@gmail.com>
2022-07-11 22:59:18 +0200

    added dropdown to navbar

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -1,36 +1,121 @@
 <template>
+  <!-- <Dropdown v-if="openProfileMenu" :anchor="$refs.anchor" /> -->
   <div
-    class="w-full max h-14 border-b-2 p-2 flex items-baseline justify-between"
+    class="w-full max h-14 border-b-2 py-2 px-4 flex items-center justify-between"
   >
-    <p class="text-xl font-semibold">{{}}</p>
-    <Toggle :checked="theme.darkMode" @click="theme.toggleTheme" />
+    <div>
+      <div v-if="currentBoard" class="flex items-center text-slate-600 gap-2">
+        <p class="text-xl font-semibold">{{ `${currentBoard.name}` }}</p>
+        <button @click="ds">
+          <CogIcon class="h-5 w-5" />
+        </button>
+      </div>
+    </div>
+    <p class="text-xl font-semibold">{{ `${auth.user.name}'s Workspace` }}</p>
+    <div class="flex items-center gap-6">
+      <TextButton>Add task</TextButton>
+      <Dropdown>
+        <template #button>
+          <button
+            class="bg-slate-200 focus:bg-slate-300 p-2 rounded-full flex justify-center items-center"
+          >
+            <UserIcon class="w-4 h-4" />
+          </button>
+        </template>
 
-    <TextButton @click="logout"> {{ "Log out" }}</TextButton>
+        <template #menu>
+          <button
+            v-for="item in menu"
+            :key="item.name"
+            :ref="item.ref"
+            class="w-40 flex justify-between items-center py-2 px-4 hover:bg-slate-100"
+            @click="handleClick(item)"
+          >
+            <p>{{ item.text }}</p>
+            <component :is="item.icon" class="w-4 h-4" />
+          </button>
+        </template>
+      </Dropdown>
+    </div>
   </div>
 </template>
 
 <script>
-import { useAuth } from "../stores/auth"
+import { animate } from "motion"
 import { useStore } from "../stores/store"
-import { useThemeStore } from "../stores/theme"
+import { useAuth } from "../stores/auth"
 import TextButton from "./buttons/TextButton.vue"
 import Toggle from "./buttons/ThemeToggle.vue"
+import {
+  LogoutIcon,
+  UserIcon,
+  CogIcon,
+  ColorSwatchIcon,
+} from "@heroicons/vue/outline"
+import Dropdown from "./menu/Dropdown.vue"
 
 export default {
-  components: { TextButton, Toggle },
+  components: { TextButton, Toggle, UserIcon, Dropdown, CogIcon },
+
   data: () => ({
     store: useStore(),
     auth: useAuth(),
-    theme: useThemeStore(),
-    currentBoard: null,
+
+    menu: [
+      {
+        text: "Profile",
+        ref: "profile",
+        action: () => {
+          this.toUser
+        },
+        icon: ColorSwatchIcon,
+      },
+
+      {
+        text: "Settings",
+        ref: "settings",
+        action: () => {
+          this.toSettings
+        },
+        icon: CogIcon,
+      },
+
+      {
+        text: "Log out",
+        ref: "logout",
+        action: () => {
+          this.logout
+        },
+        icon: LogoutIcon,
+      },
+    ],
   }),
+
   computed: {
     id() {
       return this.$route.params.id
     },
+
+    currentBoard() {
+      return this.store.currentBoard
+    },
   },
 
   methods: {
+    handleClick(item) {
+      animate(this.$refs[item.ref], { scale: [1, 1.1, 1] })
+
+      item.action()
+    },
+
+    toUser() {
+      this.$router.replace("/user")
+    },
+
+    toSettings() {
+      this.$router.replace("/user/settings")
+    },
+
     logout() {
       this.auth.logout()
       this.$router.replace("/")
---
+++ client/src/components/menu/Dropdown.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="relative" @click="open = !open">
+    <slot name="button" />
+    <Presence>
+      <Motion
+        v-show="open"
+        :initial="{ opacity: 0 }"
+        :animate="{ opacity: 1 }"
+        :exit="{ opacity: 0 }"
+      >
+        <div
+          class="absolute bg-white top-9 right-4 rounded-md border"
+          @click.stop
+        >
+          <ul ref="list" class="flex flex-col w-full">
+            <slot name="menu" />
+          </ul>
+        </div>
+      </Motion>
+    </Presence>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    open: false,
+  }),
+
+  // methods: {
+  //   handleOpen() {
+  //     const list = this.$refs["list"]
+
+  //     animate(list, { opacity: [0, 1] })
+
+  //     this.open = !this.open
+  //   },
+  // },
+}
+</script>
