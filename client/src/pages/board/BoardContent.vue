0be1451f40746495450fe8f34f6ec2749dbda51d
Lars Djursner <larsdjursner@gmail.com>
2022-07-21 16:52:17 +0200

    removed commented

--- client/src/pages/board/BoardContent.vue
+++ client/src/pages/board/BoardContent.vue
@@ -91,39 +91,3 @@ export default {
   },
 }
 </script>
-
-<!-- <template>
-  <div class="h-full w-full overflox-x-scroll px-6 py-2 bg-slate-100">
-    <ul v-if="currentBoard && currentBoard.stories" class="flex gap-10">
-      <li
-        v-for="task in currentBoard.stories"
-        :key="task.id"
-        class="rounded-lg px-4 py-2 border shadow-lg bg-white"
-      >
-        {{ task.name }}
-      </li>
-    </ul>
-
-    <div v-else>loading.....</div>
-  </div>
-</template>
-
-<script>
-import { useStore } from "../../stores/store"
-
-export default {
-  props: {
-    id: { type: Number, required: true },
-  },
-
-  data: () => ({
-    store: useStore(),
-  }),
-
-  computed: {
-    currentBoard() {
-      return this.store.currentBoard
-    },
-  },
-}
-</script> -->
