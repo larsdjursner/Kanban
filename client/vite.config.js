c84e69d7ede695b5da832a830f6ed8cd0ab3e775
Lars Djursner <larsdjursner@gmail.com>
2022-08-21 21:57:31 +0200

    src alias

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,15 @@
 import { defineConfig } from "vite"
 import vue from "@vitejs/plugin-vue"
+import { resolve } from "path"
 import eslintPlugin from "vite-plugin-eslint"
 
 // https://vitejs.dev/config/
+/** @type {import('vite').UserConfig} */
 export default defineConfig({
   plugins: [vue(), eslintPlugin()],
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "./src"),
+    },
+  },
 })
